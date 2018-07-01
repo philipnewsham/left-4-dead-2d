a8852d6287e57b5affc6339f106a9b47776bbb03
PhilipN <philip.newsham@hotmail.co.uk>
2018-07-01 14:58:50 +0100

    added weapon xml

---
+++ Left4Dead2D/Assets/Scripts/Weapon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.Xml.Serialization;
+
+public static class Slot
+{
+    public enum SlotType
+    {
+        Primary,
+        Secondary,
+        Throwable
+    }
+}
+
+[System.Serializable]
+public class Weapon
+{
+    [XmlAttribute("name")]
+    public string weaponName;
+    public Slot.SlotType slotType;
+    public int maxAmmo;
+    public int clipSize;
+}
---
+++ Left4Dead2D/Assets/Scripts/WeaponContainer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
+
+[XmlRoot("WeaponCollection")]
+public class WeaponContainer
+{
+    [XmlArray("Weapons")]
+    [XmlArrayItem("Weapon")]
+    public List<Weapon> weapons = new List<Weapon>();
+
+    public void Save(string path)
+    {
+        var serializer = new XmlSerializer(typeof(WeaponContainer));
+        using (var stream = new FileStream(path, FileMode.Create))
+        {
+            serializer.Serialize(stream, this);
+        }
+    }
+
+    public static WeaponContainer Load(string path)
+    {
+        var serializer = new XmlSerializer(typeof(WeaponContainer));
+        using (var stream = new FileStream(path, FileMode.Open))
+        {
+            return serializer.Deserialize(stream) as WeaponContainer;
+        }
+    }
+
+    //Loads the xml directly from the given string. Useful in combination with www.text.
+    public static WeaponContainer LoadFromText(string text)
+    {
+        var serializer = new XmlSerializer(typeof(WeaponContainer));
+        return serializer.Deserialize(new StringReader(text)) as WeaponContainer;
+    }
+}
