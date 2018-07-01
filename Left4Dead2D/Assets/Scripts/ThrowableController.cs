94ac7d55120d2c6c91d6fd505356fbb4d4791669
PhilipN <philip.newsham@hotmail.co.uk>
2018-07-01 18:45:22 +0100

    throwable test

---
+++ Left4Dead2D/Assets/Scripts/ThrowableController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowableController : MonoBehaviour
+{
+    public GameObject molotov;
+    public float force;
+    public Transform parent;
+
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject grenadeClone = Instantiate(molotov, transform);
+            grenadeClone.transform.SetParent(parent);
+            Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            diff.Normalize();
+            float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+            grenadeClone.transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90.0f);
+
+            grenadeClone.GetComponent<Rigidbody2D>().AddRelativeForce(grenadeClone.transform.up * force);
+        }
+	}
+}
